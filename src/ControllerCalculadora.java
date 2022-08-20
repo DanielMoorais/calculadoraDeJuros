49b19976b88d55ad11b3b54e7fbfcbf29f832f78
Daniel Morais <ielmorais@hotmail.com>
2022-08-20 08:22:46 -0300

    PrimeiraVersao

---
+++ src/ControllerCalculadora.java
@@ -0,0 +1,22 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class ControllerCalculadora {
+
+    @FXML
+    private Slider sliderParcelas;
+
+    @FXML
+    private TextField textFieldJuros;
+
+    @FXML
+    private TextField textFieldParcela;
+
+    @FXML
+    private TextField textFieldTotal;
+
+    @FXML
+    private TextField textFieldValor;
+
+}
