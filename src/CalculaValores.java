56d71640bb761f2556d053486445d98be75c9434
Daniel Morais <ielmorais@hotmail.com>
2022-08-23 23:50:48 -0300

    V2

--- src/CalculaValores.java
+++
@@ -1,9 +0,0 @@
-public class CalculaValores {
-
-    public double calculaJuros(double valor, double juros){
-        double resultado;
-        resultado = valor * juros;
-        return resultado;
-    }
-    
-}
